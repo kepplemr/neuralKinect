Fixed corrupted .csv's, backpropagation training up

neuralkinect.py
@@ -1,31 +1,45 @@
-'''
-Created on Dec 14, 2013
-
-@author: Kep
+#!/usr/bin/env python
+''' 
+    @title ->          neuralkinect.py
+    @author ->         Michael Kepple
+    @date ->           15 Dec 2013
+    @description ->    neuralkinect.py -> Achieves Kinect gesture recognition
+                       through training a neural network for classification.
+    @note ->           Utilizes SciPy && PyBrain libraries:
+                       http://www.scipy.org/install.html
+                       https://github.com/pybrain/pybrain
+    @python_version -> Anaconda (2.7)
+    @usage ->          python neuralkinect.py
 '''
 import csv
 import glob
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure.modules   import SoftmaxLayer
 
 class NeuralKinect():
-    
     def __init__(self):
-        self.neuralNet = buildNetwork(60, 20, 5)
+        # Softmax layer -> great for classification networks
+        self.neuralNet = buildNetwork(60, 20, 5, outclass=SoftmaxLayer)
         self.dataSet = SupervisedDataSet(60, 5)
-        
-    def initWeights(self):
-        pass
-    
-    def trainNetwork(self):
+
+    def trainBackProp(self):
+        trainer = BackpropTrainer(self.neuralNet, self.dataSet)
+        print(repr(trainer.train()))
+
+    def loadDataSet(self):
         points = []
         for csvFile in glob.iglob("TrainData/*.csv"):
             with open(csvFile, 'rt') as letterSet:
                 reader = csv.reader(letterSet)
                 header = str(reader.next())
                 letter = header[2:3]
-                target = header[4:9]
-                print("Training DataSet: " + letter)
+                targetStr = header[4:9]
+                print("Processing Dataset for letter -> " + letter)
+                target = []
+                for digit in targetStr:
+                    target.append(digit)
                 rows = 1
                 for row in reader:              
                     for col in row:
@@ -37,7 +51,8 @@ class NeuralKinect():
 
 def main():
     nk = NeuralKinect()
-    nk.trainNetwork()
-        
+    nk.loadDataSet()
+    nk.trainBackProp()
+    
 if __name__ == '__main__':
     main()